d71388da673a07dec8b0bae3398f376d66c5501a
Jaired Jawed <jairedjawed@icloud.com>
2021-04-11 22:31:32 -0700

    Update Part4

--- test/tests.py
+++ test/tests.py
@@ -40,5 +40,5 @@ tests = [
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['PORTB']
+watch = ['PORTD']
 
